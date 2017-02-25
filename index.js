53078e0d7ce6871ec4f579326d29c75e8bb2710c
Walidmash <waleedmash2@hotmail.com>
2017-02-25 18:19:03 +0200

    creat index.html file

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 'use strict';
 
 var http = require('http');
+var fs = require('fs');
+
+var html = fs.readFileSync(__dirname + '/index.html','utf8');
 
 http.createServer(function(req,res){
-  res.end('hello');
+  res.end(html);
 }).listen(process.env.PORT || 8000 , function(){
   console.log("Server is Running!");
 });
