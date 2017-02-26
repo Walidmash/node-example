85ef23fdb9e690a7cd00cd77b344a10b150d690c
besart <besartshyti@gmail.com>
2017-02-26 19:00:17 +0200

    test CI and CD

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 var http = require('http');
 var fs = require('fs');
-
 var html = fs.readFileSync(__dirname + '/index.html','utf8');
+var PORT = 8000;
 
 http.createServer(function(req,res){
   res.end(html);
-}).listen(process.env.PORT || 8000 , function(){
-  console.log("Server is Running!");
+}).listen(process.env.PORT || PORT , function() {
+  console.log(`Server running on ${PORT}`);
 });
